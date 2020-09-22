acc29f44a142c1f98540db91b9e108fa85b3bd8c
Vinicius Fritzen Machado <fritzenvinicius@gmail.com>
2020-09-22 19:26:02 -0300

    Primeiro Commit - Códigos iniciais

---
+++ Semana01/AumentaPorcento.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AumentaPorcento  {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        
+        float valor, porcentagem, valorAtualizado;
+        
+        System.out.println("Informe um valor");
+        valor = in.nextFloat();
+
+        System.out.println("Aumento de quantos %?");
+        porcentagem = in.nextFloat();
+
+        valorAtualizado = valor * (1 + porcentagem/100);
+
+        System.out.println(valor + " atualizando em " + porcentagem + " e' igual a " + valorAtualizado);
+
+    }
+}
+
